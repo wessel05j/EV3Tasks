41200a94c3c1cc2b62f3f48ccc6f4fb3bc567dee
EmilR <emilrim@gmail.com>
2025-10-09 08:58:00 +0200

    Added all code from 'Lego Project 3'  into 'oppgave3' and into repository

--- oppgave3/main.py
+++ oppgave3/main.py
@@ -6,18 +6,55 @@ from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
+import random
+# Initialiser EV3-brikken (hovedenhenten til roboten) 
+hjernen = EV3Brick()
 
+# Initialiser motorene på port B og C (venstre og høyre hjul)
+venstre_motor = Motor(Port.A)
+hoyre_motor = Motor(Port.D)
 
-# This program requires LEGO EV3 MicroPython v2.0 or higher.
-# Click "Open user guide" on the EV3 extension tab for more information.
+#kommand for å styre både høyre og venstre motor som 'robot', må ha størrelse og avstand mellom hjul i tilegg.
+robot = DriveBase(hoyre_motor, venstre_motor, wheel_diameter=56, axle_track=114)
 
+#sensorene
+farge_sensor = ColorSensor(Port.S1)
+ultra = UltrasonicSensor(Port.S3)
 
-# Create your objects here.
-ev3 = EV3Brick()
+#definerer 'klokke' for å holde styr på tid 
+klokke = StopWatch()
 
+#tilfeldig ting roboten gjør for å 'underholde'
+moro = [
+    lambda: hjernen.speaker.play_file(SoundFile.HELLO),
+    lambda: hjernen.speaker.play_file(SoundFile.HORN_1),
+    lambda: hjernen.speaker.play_file(SoundFile.MAGIC_WAND),
+    lambda: hjernen.screen.print("Hei der!"),
+]
 
-# Write your program here.
-ev3.speaker.beep()
+while True:
+    # Eksempel på enkel linjefølger
+    if farge_sensor.reflection() < 20: # mørkt underlag, dårlig refleksjon/lys 
+        venstre_motor.run(200)
+        hoyre_motor.run(100)
+    else:
+        venstre_motor.run(50)
+        hoyre_motor.run(300)                                                                     
 
+    #Klokke for å underholde hvert 10 sekund
+    if klokke.time() > 10000: # 10 sekunder
+        robot.stop()
+        random.choice(moro)() # velger en tilfeldig funksjon fra 'moro' listen og kjører den
+        wait(1000)
+        klokke.reset()
 
-#Forventet at Emil skal legge inn kode.
+    #Stopper robot da hinderet er nærmere enn 10 cm, og spiller av 'cheering' lyd
+    if ultra.distance() < 100:  # 10 cm
+        robot.stop()
+        hjernen.speaker.play_file(SoundFile.CHEERING)
+        wait(1000)
+        break
+
+    #funksjon for å stoppe programmet utenom å bruke hinderen (i tilfølge noe går galt)
+    if Button.CENTER in hjernen.buttons.pressed():
+        break
